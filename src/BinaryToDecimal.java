3d5d0a1d94f55bbd830861c8d07421b6906219f4
Vittorio Longo <vittorio.longo@itsrizzoli.it>
2023-12-18 11:22:01 +0100

    funzione di conversione

--- src/BinaryToDecimal.java
+++ src/BinaryToDecimal.java
@@ -2,15 +2,18 @@ import java.util.Scanner;
 
 public class BinaryToDecimal {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner (System.in);
-
-        System.out.println("Inserisci una stringa binaria:");
-        String binaryString = scanner.nextLine();
-
+        System.out.println("test");
+    }
+    public static  int binaryToDecimal (String binaryString){
         int decimalValue = 0;
-        //int decimalValue = binaryToDecimal(binaryString);
 
-        System.out.println("Valore decimale" +decimalValue);
-        scanner.close();
+        int len = binaryString.length();
+
+        for(int i = 0; i < len ; i++){
+            int digit = Character.getNumericValue(binaryString.charAt(i))
+            int power = len - i - 1;
+            decimalValue = decimalValue + (int) (digit * Math.pow (2, power));
+            }
+        return decimalValue;
     }
 }
